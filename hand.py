08c1555f552f1108f1a2598d06b885a2c8e4302f
patrickaod <131899922+patrickaod@users.noreply.github.com>
2026-05-02 18:45:09 +0100

    Refactor: change hand.display() to return output instead of printing

--- hand.py
+++ hand.py
@@ -28,15 +28,15 @@ class Hand:
         return self.get_value()==21
 
     def display(self, show_all_dealer_cards=False):
-        print(f'''{"Dealer's" if self.dealer else "Your"} Hand: ''')
+        output = f'''{"Dealer's" if self.dealer else "Your"} Hand: \n'''
 
         for index, card in enumerate(self.cards):
-            if index == 0 and self.dealer \
-                and not show_all_dealer_cards and not self.is_blackjack():
-                print("Hidden")
+            if index == 0 and self.dealer and not show_all_dealer_cards and not self.is_blackjack():
+                output += "Hidden\n"
             else:
-                print(card)
+                output += f"{card}\n"
 
         if not self.dealer:
-            print("Value:", self.get_value())
-            print()
+            output += f"Value: {self.get_value()}\n"
+
+        return output
