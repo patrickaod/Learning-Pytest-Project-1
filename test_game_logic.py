b9b3bf07f255036b78299b022b37cfa0955260bc
patrickaod <131899922+patrickaod@users.noreply.github.com>
2026-05-08 13:52:09 +0100

    Refactor: add fixtures to replace repetition

--- test_game_logic.py
+++ test_game_logic.py
@@ -2,112 +2,39 @@ from hand import Hand
 from card import Card
 from game_logic import check_winner, handle_result
 
-def test_player_bust():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_player_bust(player_bust_hand, dealer_low_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Spades", {"rank": "Q", "value": 10}),
-        Card("Clubs", {"rank": "5", "value": 5}),
-    ])
-
-    dealer_hand.add_card([
-        Card("Diamonds", {"rank": "9", "value": 9})
-    ])
-
-    result = check_winner(player_hand, dealer_hand)
+    result = check_winner(player_bust_hand, dealer_low_hand)
 
     assert result == "You bust dealer wins!"
 
-def test_dealer_bust():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_dealer_bust(player_low_hand, dealer_bust_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "5", "value": 5})
-    ])
-
-    dealer_hand.add_card([
-        Card("Diamonds", {"rank": "9", "value": 9}),
-        Card("Spades", {"rank": "Q", "value": 10}),
-        Card("Clubs", {"rank": "5", "value": 5})
-    ])
-
-    result = check_winner(player_hand, dealer_hand)
+    result = check_winner(player_low_hand, dealer_bust_hand)
 
     assert result == "Dealer bust player wins!"
 
-def test_tie_blackjack():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_tie_blackjack(blackjack_hand, dealer_blackjack_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "A", "value": 11})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "A", "value": 11})
-    ])
-
-    result = check_winner(player_hand, dealer_hand)
+    result = check_winner(blackjack_hand, dealer_blackjack_hand)
 
     assert result == "It's a Tie!"
 
-def test_player_blackjack():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_player_blackjack(blackjack_hand, dealer_high_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "A", "value": 11})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "1", "value": 1})
-    ])
-
-    result = check_winner(player_hand, dealer_hand)
+    result = check_winner(blackjack_hand, dealer_high_hand)
 
     assert result == "You have a blackjack, you WIN!"
 
-def test_dealer_blackjack():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_dealer_blackjack(player_high_hand, dealer_blackjack_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "5", "value": 5})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "A", "value": 11})
-    ])
-
-    result = check_winner(player_hand, dealer_hand)
+    result = check_winner(player_high_hand, dealer_blackjack_hand)
 
     assert result == "Dealer has a blackjack, you Lose!"
 
-def test_player_value_win():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_player_value_win(player_high_hand, dealer_low_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "9", "value": 9})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "8", "value": 8})
-    ])
-
-    result = check_winner(player_hand, dealer_hand, True)
+    result = check_winner(player_high_hand, dealer_low_hand, True)
 
     assert result == "You Win!"
 
@@ -129,39 +56,15 @@ def test_tie_hand():
 
     assert result == "TIE!"
 
-def test_dealer_value_win():
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_dealer_value_win(player_low_hand, dealer_high_hand):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "8", "value": 8})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "9", "value": 9})
-    ])
-
-    result = check_winner(player_hand, dealer_hand, True)
+    result = check_winner(player_low_hand, dealer_high_hand, True)
 
     assert result == "Dealer Win!"
 
-def test_handle_result_outputs_message(capsys):
-    player_hand = Hand()
-    dealer_hand = Hand(dealer=True)
+def test_handle_result_outputs_message(player_low_hand, dealer_high_hand,capsys):
 
-    player_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "8", "value": 8})
-    ])
-
-    dealer_hand.add_card([
-        Card("Hearts", {"rank": "K", "value": 10}),
-        Card("Hearts", {"rank": "9", "value": 9})
-    ])
-
-    print_test = handle_result(player_hand, dealer_hand, True)
+    print_test = handle_result(player_low_hand, dealer_high_hand, True)
 
     captured = capsys.readouterr()
 
